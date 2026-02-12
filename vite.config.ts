e215e3e8da8865b60bff96eadd1fa6895bac1731
guswlsdud0216 <guswlsdud0216@gmail.com>
2026-02-12 03:33:35 +0000

    feat: Apply all current changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +13,10 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
+  },
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+    ],
   },
 })
